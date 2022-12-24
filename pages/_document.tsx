97b84555643b7df043cb608c56f8731fc7d02c23
nanahiryu <ryu2751x7@gmail.com>
2022-12-24 19:08:55 +0900

    header定義の移動

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { Header } from "../components/organisms/Header";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Header />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
