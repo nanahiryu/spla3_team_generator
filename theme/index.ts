8ecaf0bb49a799e07b52b8e3aa4aa511cf3cb0f5
nanahiryu <ryu2751x7@gmail.com>
2022-12-24 17:30:14 +0900

    splaのカラーテーマ追加

---
+++ theme/index.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    splaTarcoids: "#06c9b3",
+    splaLimeGreen: "#a9df03",
+    splaPurple: "#ca00de",
+    splaPink: "#fd2996",
+    splaOrange: "#f98620",
+    splaBlue: "#1812ce",
+  },
+});
