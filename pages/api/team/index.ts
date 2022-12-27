6adef9ddccd469077e59dfb4a7001db224db8202
nanahiryu <ryu2751x7@gmail.com>
2022-12-27 22:30:41 +0900

    api仮実装

---
+++ pages/api/team/index.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Player } from "../../../types/RankCard";
+import { ReqTeamGrouping } from "../../../types/request/Team";
+import { RespTeamGrouping } from "../../../types/response/Team";
+
+const teamApi = (
+  req: NextApiRequest,
+  res: NextApiResponse<RespTeamGrouping>
+) => {
+  const bravoMembers: Player[] = [];
+  const alphaMembers: Player[] = [];
+  const members: ReqTeamGrouping = req.body;
+  console.log(members);
+  let playerId = 0;
+  members.RankMembers.forEach((rank) => {
+    rank.userList.forEach((user) => {
+      if (bravoMembers.length <= alphaMembers.length) {
+        bravoMembers.push({ playerId: playerId, playerName: user });
+      } else {
+        alphaMembers.push({ playerId: playerId, playerName: user });
+      }
+      playerId++;
+    });
+  });
+  res.status(200).json({ bravo: bravoMembers, alpha: alphaMembers });
+};
+
+export default teamApi;
