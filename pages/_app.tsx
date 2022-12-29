c0bd6453f0c464c0bb5a683e913a0c8b61fb6b24
nanahiryu <ryu2751x7@gmail.com>
2022-12-29 19:18:11 +0900

    auth関連の削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,34 +10,6 @@ import { useLoadings } from "../Hooks/useLoadings";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { pathname, push } = useRouter();
-  const { loading, startLoading, stopLoading } = useLoadings();
-
-  supabase.auth.onAuthStateChange((_, session) => {
-    console.log(session);
-    if (session?.user && (pathname === "/signin" || pathname === "/signup")) {
-      push("/");
-    } else if (!session?.user && pathname !== "/signup") {
-      push("/signin");
-    }
-  });
-
-  useEffect(() => {
-    (async () => {
-      startLoading();
-      const userData = await supabase.auth.getUser();
-      // userが存在かつ, signin, signupページにいる場合は, "/"にpush
-      const user = userData.data?.user;
-      console.log(user);
-      if (user && (pathname === "/signin" || pathname === "/signup")) {
-        await push("/");
-      } else if (!user && pathname !== "/signup") {
-        await push("/signin");
-      }
-      stopLoading();
-    })();
-  }, []);
-
   return (
     <ChakraProvider theme={theme}>
       <Head>
