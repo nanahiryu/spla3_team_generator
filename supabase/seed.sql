b6f5f9517a792df032916da1aa79de2b730fa09b
nanahiryu <ryu2751x7@gmail.com>
2023-02-20 00:31:10 +0900

    カラーテーマを変更

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,6 +1,7 @@
 INSERT INTO public.ranks (name, rank_color)
 VALUES
-('X', 'splaBlue'),
-('S', 'splaLimeGreen'),
-('A', 'splaTarcoids'),
-('B', 'splaOrange');
+('X', 'splaPurple'),
+('S', 'splaPink'),
+('A', 'splaOrange'),
+('B', 'splaYellow'),
+('C', 'splaLimeGreen');
