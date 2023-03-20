1796438050884eb5580175f571bdc680f737407c
nanahiryu <ryu2751x7@gmail.com>
2023-03-20 13:00:20 +0900

    indexページのリファクタ

--- features/Index/index.tsx
+++ features/Index/index.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   Flex,
+  FormControl,
   Input,
   List,
   ListItem,
@@ -9,7 +10,7 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { useAuthUser } from "../../Atoms";
 import { useLoadings } from "../../Hooks/useLoadings";
 import { LoadSpinner } from "../../components/atoms/loadSpinner";
@@ -30,7 +31,7 @@ const DescriptionCard = (props: DescriptionCardProps) => {
       bg="white"
       py="2"
       px="6"
-      w="30%"
+      w="45%"
       justify="start"
       align="center"
       borderRadius="xl"
@@ -38,14 +39,16 @@ const DescriptionCard = (props: DescriptionCardProps) => {
       <Text fontSize="xl" fontWeight="semibold" my="4">
         {title}
       </Text>
-      <Box borderRadius="xl" overflow="hidden" h="220px">
-        <Image
-          src={fig_path}
-          alt=""
-          width={400}
-          height={200}
-          style={{ objectFit: "cover" }}
-        />
+      <Box h="220px">
+        <Box borderRadius="xl" overflow="hidden" maxH="200px">
+          <Image
+            src={fig_path}
+            alt=""
+            width={400}
+            height={200}
+            style={{ objectFit: "cover" }}
+          />
+        </Box>
       </Box>
       <Box m="4">
         <Text fontSize="lg" fontWeight="bold">
@@ -70,7 +73,8 @@ const Home = () => {
   //   console.log(userAuth);
   // });
 
-  const onClickCreateNewGroup = async () => {
+  const onClickCreateNewGroup = async (event: FormEvent) => {
+    event.preventDefault();
     try {
       // groupNameが空またはスペースのみの場合はthrow
       if (!groupName || !groupName.trim()) {
@@ -107,23 +111,33 @@ const Home = () => {
             <Box fontSize="3xl" fontWeight="semibold" mt="12">
               グループ名を入力してspla team generatorを始める
             </Box>
-            <Flex flexDirection="column" gap="4" w="80">
-              <Input
-                placeholder="グループの名前を入力"
-                value={groupName}
-                onChange={(e) => onChangeGroupName(e)}
-                bgColor="white"
-                size="lg"
-              />
-              <Button
-                bgColor="teal.400"
-                color="white"
-                size="lg"
-                onClick={onClickCreateNewGroup}
-              >
-                グループを作成する
-              </Button>
+            <Flex
+              as="form"
+              w="full"
+              justify="center"
+              align="center"
+              onSubmit={onClickCreateNewGroup}
+            >
+              <FormControl display="flex" flexDirection="column" w="80" gap="4">
+                <Input
+                  placeholder="グループの名前を入力"
+                  value={groupName}
+                  onChange={(e) => onChangeGroupName(e)}
+                  bgColor="white"
+                  size="lg"
+                />
+                <Button
+                  type="submit"
+                  bgColor="teal.400"
+                  color="white"
+                  size="lg"
+                  onClick={onClickCreateNewGroup}
+                >
+                  グループを作成する
+                </Button>
+              </FormControl>
             </Flex>
+
             <Box fontSize="3xl" fontWeight="semibold" mt="24">
               spla team generator とは
             </Box>
@@ -159,21 +173,26 @@ const Home = () => {
                 spla team generator でできること
               </Text>
             </Box>
-            <Flex gap="4" w="90%" justify="center">
+            <Flex gap="4" w="90%" justify="center" flexWrap="wrap">
               <DescriptionCard
                 title="1. チーム名を入力"
                 description="以下のボックスにグループ名を入力し、グループを作成しましょう。"
-                fig_path="/img/splatoon_train_bg.jpg"
+                fig_path="/img/input_team_name.png"
               />
               <DescriptionCard
                 title="2. チームメンバーをランクごとに登録"
                 description="チームメンバーをウデマエごとに登録しましょう。(ゲーム内のウデマエと一致させる必要はありません。)"
-                fig_path="/img/udemae_bg.jpeg"
+                fig_path="/img/input_player_name.png"
               />
               <DescriptionCard
                 title="3. 何度でもチームを生成"
                 description="チームメンバーを登録したら、何度でもチームを生成することができます。二回目以降は、登録したチームメンバーがなるべく被らないようにチームが組まれます。"
-                fig_path="/img/private_match_bg.jpg"
+                fig_path="/img/output_team.png"
+              />
+              <DescriptionCard
+                title="4. urlを共有"
+                description="サイドバーのネームタグをクリックすることでurlを保存, 共有でき, チームメンバーを再入力する手間が省けます。"
+                fig_path="/img/copy_team_name.png"
               />
             </Flex>
           </>
